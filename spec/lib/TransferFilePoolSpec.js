fa5b5e3a489b5596380ab1d9c521c51f36ff8e93
Ludovic Muller <ludovicmuller1@gmail.com>
2022-03-15 21:22:31 +0100

    tests: check with and without retries

--- spec/lib/TransferFilePoolSpec.js
+++ spec/lib/TransferFilePoolSpec.js
@@ -263,4 +263,54 @@ describe("testing the TransferFilePool class", () => {
     // check our counter
     expect(counter).toEqual(3); // should ask parts: 0, 5 and 10 => 3 requests
   });
+
+  it("should send the file with exactly one retry", async () => {
+    let counter = 0;
+
+    /**
+     * SENDER
+     */
+    const senderPool = new TransferFilePool({});
+    const file = new Blob(["Hello world!"], {
+      type: "text/plain",
+    });
+    const fileMetadata = await senderPool.addFile(file, "test.txt");
+
+    /**
+     * RECEIVER
+     */
+    let sendCb = (_fileId, _offset, _limit, _data) => {};
+    const receiverPool = new TransferFilePool({
+      maxBufferSize: 5,
+      parallelCalls: 100,
+      askFilePartCallback: async (fileId, offset, limit) => {
+        counter++;
+        // imagine the receiver sending a message to the sender to ask this part of this file…
+
+        // sender part:
+        const partData = senderPool.readFilePart(fileId, offset, limit);
+
+        // imagine the sender sends the data to the receiver…
+        await new Promise(r => setTimeout(r, 2000));
+
+        // receiver part:
+        sendCb(fileId, offset, limit, partData);
+      }
+    });
+    sendCb = (fileId, offset, limit, data) => {
+      receiverPool.receiveFilePart(fileId, offset, limit, data);
+    };
+
+    // imagine the sender sent the fileMetadata on a dedicated channel…
+    const receivedFileMetadata = {...fileMetadata};
+
+    // so we store the file metadata in the pool
+    receiverPool.storeFileMetadata(receivedFileMetadata);
+
+    // imagine the user click on the download button
+    await receiverPool.downloadFile(receivedFileMetadata.id);
+
+    // check our counter
+    expect(counter).toEqual(6); // should ask parts: 0, 5 and 10 => 3 requests * 2 (for 1 retry)
+  });
 });
