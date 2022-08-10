59fd54e7f9be985b2482a7509e9e4de526644544
Ludovic Muller <ludovicmuller1@gmail.com>
2022-08-10 23:03:48 +0200

    test: clear file

--- spec/lib/TransferFilePoolSpec.js
+++ spec/lib/TransferFilePoolSpec.js
@@ -113,7 +113,7 @@ describe("testing the TransferFilePool class", () => {
     await expectAsync(receiverPool.downloadFile(fileId)).toBeRejectedWithError(`file '#${fileId}' does not exist`);
   });
 
-  it("should work be able to send a file", async () => {
+  it("should be able to send a file", async () => {
     /**
      * SENDER
      */
@@ -161,7 +161,7 @@ describe("testing the TransferFilePool class", () => {
     expect(finalContent).toEqual("Hello world!");
   });
 
-  it("should work have the same amount of ask and sent parts", async () => {
+  it("should have the same amount of ask and sent parts", async () => {
     let askCounter = 0;
     let sentCounter = 0;
 
@@ -464,5 +464,62 @@ describe("testing the TransferFilePool class", () => {
     const downloadPromise = receiverPool.downloadFile(receivedFileMetadata.id);
     receiverPool.abortFileDownload(receivedFileMetadata.id);
     await expectAsync(downloadPromise).toBeRejectedWithError("download aborted");
+  });
+
+  it("should be able to clear a file", async () => {
+    /**
+     * SENDER
+     */
+    const senderPool = new TransferFilePool({});
+    const file = new Blob(["Hello world!"], {
+      type: "text/plain",
+    });
+    const fileMetadata = await senderPool.addFile(file, "test.txt");
+
+    /**
+     * RECEIVER
+     */
+    let sendCb = (_fileId, _offset, _limit, _data) => {};
+    const receiverPool = new TransferFilePool({
+      maxBufferSize: 5,
+      parallelCalls: 100,
+      askFilePartCallback: async (fileId, offset, limit) => {
+        // imagine the receiver sending a message to the sender to ask this part of this file…
+
+        // sender part:
+        const partData = senderPool.readFilePart(fileId, offset, limit);
+
+        // imagine the sender sends the data to the receiver…
+        await new Promise(r => setTimeout(r, Math.random() * 200));
+
+        // receiver part:
+        sendCb(fileId, offset, limit, partData);
+      }
+    });
+    sendCb = (fileId, offset, limit, data) => {
+      receiverPool.receiveFilePart(fileId, offset, limit, data);
+    };
+
+    // imagine the sender sent the fileMetadata on a dedicated channel…
+    const receivedFileMetadata = {...fileMetadata};
+
+    // so we store the file metadata in the pool
+    receiverPool.storeFileMetadata(receivedFileMetadata);
+
+    // imagine the user click on the download button
+    await receiverPool.downloadFile(receivedFileMetadata.id);
+    const finalFile = receiverPool.getFile(receivedFileMetadata.id);
+    const finalContent = await finalFile.data.text();
+    expect(finalContent).toEqual("Hello world!");
+
+    // clear the file ; it should throw an error if we try to access the file again
+    receiverPool.clearFile(receivedFileMetadata.id);
+    expect(() => receiverPool.getFile(receivedFileMetadata.id)).toThrowError("file is incomplete");
+
+    // trigger the download of the file again
+    await receiverPool.downloadFile(receivedFileMetadata.id);
+    const reDownloadedFile = receiverPool.getFile(receivedFileMetadata.id);
+    const reDownloadedFileContent = await reDownloadedFile.data.text();
+    expect(reDownloadedFileContent).toEqual("Hello world!");
   });
 });
